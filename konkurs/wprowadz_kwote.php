13d0ee82f2ef182d83d3238119bb8f4d36404bb2
AnthonyGrod <53835371+AnthonyGrod@users.noreply.github.com>
2023-02-26 01:58:42 +0100

    Update wprowadz_kwote.php

--- konkurs/wprowadz_kwote.php
+++ konkurs/wprowadz_kwote.php
@@ -4,7 +4,7 @@
 ?>
 
 <?php
-$conn = pg_connect("host=lkdb dbname=bd user=ag438477 password=030757Jg");
+$conn = pg_connect("host=lkdb dbname=bd user=ag438477 password=123");
 // Sprawdzenie poprawnosci polaczenia z baza danych zawody.
 if (!$conn) {
     echo "Wystąpił błąd podczas łączneia się z bazą.\n";
