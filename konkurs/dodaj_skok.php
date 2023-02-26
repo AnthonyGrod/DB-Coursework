ee57f9789be3705913d0b44df37bf09be9834398
AnthonyGrod <53835371+AnthonyGrod@users.noreply.github.com>
2023-02-26 01:58:14 +0100

    Update dodaj_skok.php

--- konkurs/dodaj_skok.php
+++ konkurs/dodaj_skok.php
@@ -2,7 +2,7 @@
 include_once "./func-konkurs.php";
 
 session_start();
-$conn = pg_connect("host=lkdb dbname=bd user=ag438477 password=030757Jg");
+$conn = pg_connect("host=lkdb dbname=bd user=ag438477 password=123");
 // Sprawdzenie poprawnosci polaczenia z baza danych zawody.
 if (!$conn) {
     echo "Wystąpił błąd podczas łączneia się z bazą.\n";
